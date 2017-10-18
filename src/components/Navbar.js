7bf31b8d713b2c01ec54543108aaa5792bc64575
Rendy Febry <rendyfebry@gmail.com>
2017-10-18 16:58:21 +0700

    Fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,30 +13,28 @@ export default class Example extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<Navbar color="faded" light expand="md">
-					<Link className="navbar-brand" to="/" href="/">Alkemi</Link>
-					<NavbarToggler onClick={this.toggle} />
-					<Collapse isOpen={this.state.isOpen} navbar>
-						<Nav className="ml-auto" navbar>
-							<NavItem>
-								<Link className="nav-link" href="/about" to="/about">About</Link>
-							</NavItem>
-							<NavItem>
-								<Link className="nav-link" href="/settings" to="/settings">Settings</Link>
-							</NavItem>
-							<NavItem>
-								<NavLink
-									href="https://github.com/rendyfebry/main-pwa"
-									target="_blank"
-								>
-									Github
-								</NavLink>
-							</NavItem>
-						</Nav>
-					</Collapse>
-				</Navbar>
-			</div>
+			<Navbar light expand="md">
+				<NavbarBrand to="/" href="/">Alkemi</NavbarBrand>
+				<NavbarToggler onClick={this.toggle} />
+				<Collapse isOpen={this.state.isOpen} navbar>
+					<Nav className="ml-auto" navbar>
+						<NavItem>
+							<Link className="nav-link" href="/about" to="/about">About</Link>
+						</NavItem>
+						<NavItem>
+							<Link className="nav-link" href="/settings" to="/settings">Settings</Link>
+						</NavItem>
+						<NavItem>
+							<NavLink
+								href="https://github.com/rendyfebry/main-pwa"
+								target="_blank"
+							>
+								Github
+							</NavLink>
+						</NavItem>
+					</Nav>
+				</Collapse>
+			</Navbar>
 		)
 	}
 }
